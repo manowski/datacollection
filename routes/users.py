9c4ecd893c9dbbb451ab97c5bfa539a92b73fdf1
manowski <manowski10@gmail.com>
2020-06-16 16:10:18 +0200

    Add top 200 users by country

--- routes/users.py
+++ routes/users.py
@@ -32,3 +32,37 @@ def handle_users():
         }
 
         return jsonify(result)
+
+# show top200 by countries
+def handle_top_country(country_alias):
+    if request.method == 'GET':
+        country_full_name = []
+        with open("countries.json") as json_file:
+            data = json.load(json_file)
+            for d in data:
+                if d['code'] == country_alias.upper():
+                    country_full_name.append(d)
+
+        users = UserModel.query.filter(UserModel.country.ilike(country_alias)).order_by(UserModel.fans.desc()).limit(200)
+
+        users_list = [
+            {
+                "name": user.name,
+                "full_name": user.full_name,
+                "user_id": user.user_id,
+                'cover_medium': user.cover_medium,
+                "fans": user.fans,
+                "following": user.following,
+                "digg": user.digg,
+                "video_count": user.video_count,
+                "hearts": user.hearts,
+
+            } for user in users]
+
+        result = {
+            "users_list": users_list,
+            "country": country_full_name,
+            "count": len(users_list)
+        }
+
+        return jsonify(result)
