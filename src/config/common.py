bdc845fcf62ef832cb7d6830730d7a4277446abc
manowski <manowski10@gmail.com>
2021-08-05 09:55:07 +0200

    Change JWT token expiry to one year

--- src/config/common.py
+++ src/config/common.py
@@ -276,8 +276,8 @@ REST_FRAMEWORK = {
 
 # JWT configuration
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
