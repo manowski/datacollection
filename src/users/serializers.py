2d1e4c135d270efd1b3cb06f2589d6734da43db1
manowski <manowski10@gmail.com>
2021-08-10 15:20:32 +0200

    Add Tiktok user media to profile

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 
 from src.users.models import TiktokUser
+from src.medias.models import TiktokUserMedia
+from src.medias.serializers import TiktokUserMediaSerializer
 from src.stats.models import TiktokUserDailyStats
 from src.stats.serializers import TiktokUserDailyStatsSerializer
 
 
 class TiktokUserSerializer(serializers.ModelSerializer):
+    media = serializers.SerializerMethodField()
     stats = serializers.SerializerMethodField()
+
+    def get_media(self, obj):
+        media = TiktokUserMedia.objects.filter(username=obj.username)[:14]
+        return TiktokUserMediaSerializer(media, source='tiktokusermedia_set', many=True).data
 
     def get_stats(self, obj):
         stats = TiktokUserDailyStats.objects.filter(username=obj.username)[:14]
@@ -16,5 +23,5 @@ class TiktokUserSerializer(serializers.ModelSerializer):
         model = TiktokUser
         fields = (
             'username', 'user_id', 'full_name', 'gender', 'description', 'external_url', 
-            'country','profile_pic', 'joined_date', 'followers_count', 'stats'
+            'country','profile_pic', 'joined_date', 'followers_count', 'media', 'stats'
         )
