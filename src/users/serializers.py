335468d9f23961fc727408b42027bc069fb19b2c
manowski <manowski10@gmail.com>
2021-08-10 10:32:28 +0200

    Add Tiktok stats serializer to user

--- src/users/serializers.py
+++ src/users/serializers.py
@@ -1,9 +1,20 @@
 from rest_framework import serializers
 
 from src.users.models import TiktokUser
+from src.stats.models import TiktokUserDailyStats
+from src.stats.serializers import TiktokUserDailyStatsSerializer
 
 
-class TiktokUserSerializer(serializers.HyperlinkedModelSerializer):
+class TiktokUserSerializer(serializers.ModelSerializer):
+    stats = serializers.SerializerMethodField()
+
+    def get_stats(self, obj):
+        stats = TiktokUserDailyStats.objects.filter(username=obj.username)[:14]
+        return TiktokUserDailyStatsSerializer(stats, source='tiktokuserdailystats_set', many=True).data
+
     class Meta:
         model = TiktokUser
-        fields = '__all__'
+        fields = (
+            'username', 'user_id', 'full_name', 'gender', 'description', 'external_url', 
+            'country','profile_pic', 'joined_date', 'followers_count', 'stats'
+        )
